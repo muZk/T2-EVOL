4ae41ff3c1f3a6cfa2fe7529ce7381f21b2ef0f1
Nicolás Gómez <neeco.gmz@gmail.com>
2014-05-26 16:29:09 -0400

    first commit

---
+++ Tarea2_evolutiva/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2_evolutiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
