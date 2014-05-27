d49278a3d13ef4ae0843c2506aa5d604fc7e7084
Nicolás Gómez <neeco.gmz@gmail.com>
2014-05-27 01:38:48 -0400

    agregue comentarios para mutacion de Tour

--- Tarea2_evolutiva/Tour.cs
+++ Tarea2_evolutiva/Tour.cs
@@ -64,11 +64,17 @@ namespace Tarea2_evolutiva
             CrossoverResult crossover = new CrossoverResult();
             crossover.offspring1 = Pmx(t1, t2, randomCity);
             crossover.offspring2 = Pmx(t2, t1, randomCity);
-
-            Console.WriteLine("RandomCity: "+randomCity);
-
             return crossover;
         }
+
+        /// <summary>
+        /// Mantiene el orden del tour <b>t1</b> desde la primera ciudad que visita hasta la ciudad <b>city</b>.
+        /// Para el resto de ciudades, las visita en el orden que lo hace el tour <b>t2</b>.
+        /// </summary>
+        /// <param name="t1">Tour principal en el que se basará el nuevo hijo</param>
+        /// <param name="t2">Tour que da el orden para las ciudades no visitadas de t1</param>
+        /// <param name="city">Ciudad para el punto de corte</param>
+        /// <returns>Tour hijo resultado</returns>
 
         private static Tour Pmx(Tour t1, Tour t2, int city)
         {
@@ -106,6 +112,11 @@ namespace Tarea2_evolutiva
 
             return offspring;
         }
+
+        /// <summary>
+        /// Devuelve una lista con las ciudades del 1 al 20
+        /// </summary>
+        /// <returns>Lista con números del 1 al 20</returns>
 
         private static List<int> GetCityList()
         {
